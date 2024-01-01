7c6e451900cb76d0012dc1470d9776ee7df42891
muhammadfaizan <muhammadfaizan027915@gmail.com>
2024-01-01 11:23:29 +0500

    [Done] Issue solved by the author of a package.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 "use client";
 
 import { Calendar } from "react-date-range";
+import 'react-date-range/dist/styles.css';
+import 'react-date-range/dist/theme/default.css'; 
 
 export default function Home() {
   function handleSelect(date: Date) {
